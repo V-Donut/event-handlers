2310d90daa041e58ab6512ac86aceda2355a6e94
V-Donut <vikaevt123@gmail.com>
2022-02-10 23:24:18 +0300

    Домашняя работа №11

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+'use strict';
 
+const btn = document.getElementById('btn');
+const input = document.getElementById('text');
+const square = document.getElementById('square');
 
+btn.addEventListener('click', function () {
+  let color = input.value;
+  square.style.backgroundColor = color;
+});
 
+const eBtn = document.getElementById('e_btn');
+eBtn.style.display = 'none';
 
+const range = document.getElementById('range');
+const circle = document.getElementById('circle');
 
+range.addEventListener('input', function (event) {
+  let percentage = event.target.value;
+  circle.style.height = percentage + '%';
+  circle.style.width = percentage + '%';
+});
