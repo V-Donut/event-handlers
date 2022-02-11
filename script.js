39037dc2125c1d0bb3b78ef909233c6131e43216
V-Donut <vikaevt123@gmail.com>
2022-02-11 23:32:15 +0300

    Внесла правки по CodeStyle

--- script.js
+++ script.js
@@ -3,17 +3,16 @@
 const btn = document.getElementById('btn');
 const input = document.getElementById('text');
 const square = document.getElementById('square');
+const range = document.getElementById('range');
+const circle = document.getElementById('circle');
+const eBtn = document.getElementById('e_btn');
 
 btn.addEventListener('click', function () {
   let color = input.value;
   square.style.backgroundColor = color;
 });
 
-const eBtn = document.getElementById('e_btn');
 eBtn.style.display = 'none';
-
-const range = document.getElementById('range');
-const circle = document.getElementById('circle');
 
 range.addEventListener('input', function (event) {
   let percentage = event.target.value;
